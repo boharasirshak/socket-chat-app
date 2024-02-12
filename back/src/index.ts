0cbc71e5424a4347f38672f38cc8113633bcbcaf
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-12 19:23:37 +0545

    fear: added support for env variables.

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,11 @@
+import * as dotenv from "dotenv";
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 
+dotenv.config();
+
+const PORT = process.env.PORT || 5500;
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
@@ -10,6 +14,6 @@ io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
 });
 
-httpServer.listen(3000, () => {
-  console.log("listening on http://localhost:3000");
+httpServer.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
