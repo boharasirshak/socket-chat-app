aeefd2afc727304e75d1fba2dd949bfaf22c2bc9
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-12 20:45:42 +0545

    feat: setup a basic message sending client

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,21 +1,47 @@
-import React, { useEffect } from "react";
-import { io } from "socket.io-client";
+import React, { useEffect, useState } from "react";
+import { Socket, io } from "socket.io-client";
+
+import { MessagePacket } from "./types/packets";
 
 import "./App.css";
 
 const App: React.FC = () => {
+  const [socket, setSocket] = useState<Socket>();
+
+  const sendMessage = () => {
+    socket?.emit("message", {
+      text: "text",
+      from: {
+        id: 1
+      },
+      to: {
+        id: 2
+      }
+    } as MessagePacket);
+  };
+
   useEffect(() => {
     const socket = io(import.meta.env.VITE_WS_BACKEND_URL);
+    setSocket(socket);
 
     socket.on("connect", () => {
       console.log("Connection made");
     });
+
+    return () => { 
+      socket.disconnect(); 
+    };
 
   }, []);
 
   return (
     <>
       Hello World
+
+
+      <button onClick={sendMessage}>
+        Send Message
+      </button>
     </>
   );
 };
