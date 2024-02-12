b7fef6e65c1342dcfd68ceb26384b1e17f3b4488
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-12 18:33:38 +0545

    init projects

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+});
+
+httpServer.listen(3000, () => {
+  console.log("listening on http://localhost:3000");
+});
