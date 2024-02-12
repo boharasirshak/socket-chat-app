968a82170c768be3a1d5ff03d5c6fc9290bcedcf
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-12 19:42:21 +0545

    refactor: added env variables support for frontend

--- front/src/vite-env.d.ts
+++ front/src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_PORT: number;
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_WS_BACKEND_URL: string;
+  readonly VITE_API_BACKEND_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
