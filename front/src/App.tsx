99d2cbe7b94d2908193aaab92decefd6b2ad2de5
Sirshak Bohara <boharasirshak101@gmail.com>
2024-02-12 19:29:32 +0545

    feat: basic websocket connection

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css';
+import React, { useEffect } from "react";
+import { io } from "socket.io-client";
 
-function App() {
+import "./App.css";
+
+const App: React.FC = () => {
+  const wsUrl = import.meta.env.WS_BACKEND_URL;
+
+  useEffect(() => {
+    const socket = io(wsUrl);
+    socket.on("connect", () => {
+      console.log("Connection made");
+    });
+
+  }, [wsUrl]);
 
   return (
     <>
       Hello World
     </>
   );
-}
+};
 
 export default App;
